c8299f52d7a3b855c630b8927c1c7b023959b9aa
monpeco <monpeco@gmail.com>
2016-09-09 02:08:05 +0000

    Add frame project of SharpDeveloper

---
+++ module9/ConsoleApplication9/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: M
+ * Date: 08-09-2016
+ * Time: 11:02 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace ConsoleApplication9
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
